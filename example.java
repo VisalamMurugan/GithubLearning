11cfddb809e544338001940f9028a53ba2e6e44c
VisalamMurugan <visalam.murugan@gmail.com>
2020-05-29 16:38:55 +0530

    V4.0
    added

--- example.java
+++ example.java
@@ -1,6 +1,5 @@
 public static void main()
 {
-system.out.println("this is master");
-  system.out.println("this is feature2");
-  system.out.println("this is feature1");
+system.out.println("this is master &F1 &F2");
+  
 }
